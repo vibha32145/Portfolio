8861f0bc899909b722e4fc296029fb9c133692f2
Vibha Gupta <vibha32145@gmail.com>
2025-10-12 17:08:25 +0530

    change in theme

--- Client/tailwind.config.cjs
+++ Client/tailwind.config.cjs
@@ -1,7 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // ✅ enable class-based dark mode
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
